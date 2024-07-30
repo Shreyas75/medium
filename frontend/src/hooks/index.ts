a93d65834d0cfa37d86d644862f8edd1120407c5
Shreyas75 <manotishreyas@gmail.com>
2024-07-30 10:35:08 +0530

    Completed Files

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -3,18 +3,41 @@ import axios from "axios";
 import { BACKEND_URL } from "../config";
 
 export interface Blog {
-    "content": string;
+    "id": string;
     "title": string;
-    "id": number
+    "content": string;
     "author": {
         "name": string
     }
 }
 
 
+export const useBlog = ({id}: {id: string}) => {
+    const [loading, setLoading] = useState(true);
+    const [blog, setBlog] = useState<Blog>();
+
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/api/v1/blog/${id}`, {
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("token")
+            }
+        })
+            .then(response => {
+                setBlog(response.data.blog);
+                setLoading(false);
+            })
+    }, [id])
+
+    return {
+        loading,
+        blog
+    }
+}
+
+
 export const useBlogs = () => {
     const [loading, setLoading] = useState(true);
-    const [blogs, setBlogs] = useState<Blog>();
+    const [blogs, setBlogs] = useState<Blog[]>([]);
 
 
     useEffect(() => {
